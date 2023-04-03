daf8c9a8bb97883f1bae4de990ac61f2c98ae07e
Aranza Alvarez <aranzaa@uark.edu>
2023-04-03 10:09:48 -0500

    updated file after class

--- updated_pinnacle_slurm.py
+++ updated_pinnacle_slurm.py
@@ -1,58 +1,45 @@
 #! /usr/bin/env python3
 
-#assignment_5
-
 # import modules
 import argparse
 
-#Defining Variables
-job_name = 'assignment_5'
-queue = 'comp 72'
-nodes = '1'
-number_of_processors = '32'
-walltime = '02'
-command = 'command1'
-
 # create an ArgumentParser object
-parser = argparse.ArgumentParser(description = "This script returns the job name of the slurm script")
+parser = argparse.ArgumentParser(description = "This script creates a SLURM file for jobs on the AHPCC cluster")
 
-# addinng positional (required) arguments
-parser.add_argument("job", help = "The job name that you want to submit", type = str)
+# add positional (required) arguments
+parser.add_argument("job_name", help = "Name of the job", type = str)
 
-# add optional arguments: : queue, number of nodes, number of processors, walltime
-# the default for 'store_true' is ... "False"
-# if 'store_true', then assign 'True' with -v or --verbose.
-parser.add_argument('-q','--queue', help = 'Print queue of job (default = simple output)', action = 'store_true') #shows arg parse that this is optional, both are recognized and equivalent
-action = 'store_true'
+# add optional arguments 
+#'--',shows arg parse that this is optional, both are recognized and equivalent
+parser.add_argument('-q','--queue', help = 'Requested queue (comp1, comp6, comp72), (default = comp72)', default = 'comp72', type = str) #shows arg parse that this is optional, both are recognized and equivalent
 
-parser.add_argument('-n','--nodes', help = 'Print number of nodes (default = simple output)', action = 'store_true') #shows arg parse that this is optional, both are recognized and equivalent
-action = 'store_true'
+parser.add_argument('-n','--nodes', help = 'Number of nodes to request (default =1)', default = '1', type = int) #shows arg parse that this is optional, both are recognized and equivalent
 
-parser.add_argument('-p','--processors', help = 'Print number of processors (default = simple output)', action = 'store_true') #shows arg parse that this is optional, both are recognized and equivalent
-action = 'store_true'
 
-parser.add_argument('-w','--walltime', help = 'Print walltime (default = simple output)', action = 'store_true') #shows arg parse that this is optional, both are recognized and equivalent
-action = 'store_true'
+parser.add_argument('-p','--processors', help = 'Number of processors to request (default = 24)', default = '24', type = int) #shows arg parse that this is optional, both are recognized and equivalent
+
+
+parser.add_argument('-w','--walltime', help = 'Length of the job (default = 72)', default = '72', type = int) #shows arg parse that this is optional, both are recognized and equivalent
+
 # parse the actual arguments
 # access argument values via 'args' variable
 args = parser.parse_args() 
 
-# print the number of nodes
-if args.job == job_name :
-    if args.nodes:
-        print('#SBATCH --nodes=', args.job, 'is:' + str(nodes))
 
-    # print the queue
-    if args.queue:
-        print('#SBATCH --partition', args.job, 'is:', queue)
-
-    # print the number of processors
-    if args.processors:
-        print('#SBATCH --tasks-per-node=', args.job, 'is:' + str(number_of_processors))
-
-    # print walltime
-    if args.walltime:
-        print('#SBATCH --time=', args.job, 'is:'  + str(walltime) + ':00:00')
-else:
-    print('invalid job_name')
-    
+print('#!/bin/bash')
+print()
+print('#SBATCH --job-name=' + args.job_name) #job name
+print('#SBATCH --partition',args.queue) #queue
+print('#SBATCH --nodes=' + str(args.nodes)) #number of nodes
+print('#SBATCH --tasks-per-node=',args.processors) #number of processors
+print('#SBATCH --time='+ str(args.walltime) + ':00:00')#walltime
+print('#SBATCH -o %j.out')
+print('#SBATCH -e %j.err')
+print('#SBATCH --mail-type=ALL')
+print('#SBATCH --mail-user=aranzaa@uark.edu')
+print()
+print('module purge') #purge modules
+print()
+print('cd $SLURM_SUBMIT_DIR')
+print()
+print('#job command here')
